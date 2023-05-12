3742ca6fc508376bf3d5f541345bf70a14568991
Oleksii Latypov <oleksii.latypov.la@gmail.com>
2023-05-12 21:08:50 +0300

    initial commit

--- src/database/connect.py
+++ src/database/connect.py
@@ -5,18 +5,6 @@ import pathlib
 from src.conf.config import settings
 
 
-# file_config = pathlib.Path(__file__).parent.parent.joinpath('conf/config.ini')
-# config = configparser.ConfigParser()
-# config.read(file_config)
-
-# username = config.get('DB', 'USER')
-# password = config.get('DB', 'PASSWORD')
-# db_name = config.get('DB', 'DB_NAME')
-# domain = config.get('DB', 'DOMAIN')
-# port = config.get('DB', 'PORT')
-#
-# SQLALCHEMY_DATABASE_URL = f'postgresql://{username}:{password}@{domain}:{port}/{db_name}'
-
 SQLALCHEMY_DATABASE_URL = settings.sqlalchemy_database_url
 
 
@@ -32,5 +20,3 @@ def get_db():
         db.close()
 
 
-# if __name__ == '__main__':
-#     print(get_db())
